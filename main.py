994be6aff8737bb2cc7f3dcfd01dba9bb6d72dca
defunsm <salmanhossain500@gmail.com>
2021-08-14 01:47:00 -0400

    csv

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import os
 
-from setup_nyc_payroll import download_data, read_from_file
+
+from setup_nyc_payroll import download_data, read_from_csv_file
 
 FISCAL_YEAR = 2020
 LIMIT = 1000000
-FILENAME = 'nyc_payroll.json'
+FILENAME = 'nyc_payroll.csv'
 
 def main():
 
     # Checks if the FILENAME has already been downloaded
     if not os.path.isfile(FILENAME):
-        download_data(FISCAL_YEAR, LIMIT, FILENAME)
+        payroll_data = download_data(FISCAL_YEAR, LIMIT, FILENAME)
     else:
         print(f"Skipping download {FILENAME} exists")
+        payroll_data = read_from_csv_file(FILENAME)
 
-    # Taking the json data from FILENAME. 
-
-    payroll_data = read_from_file(FILENAME)
-    print(type(payroll_data))
+    
+    
 
 if __name__ == '__main__':
     main()
--- setup_nyc_payroll.py
+++ setup_nyc_payroll.py
@@ -1,23 +1,25 @@
 import requests
 import json
-from typing import List
+import pandas as pd
 
+from typing import List
 from secrets import API_TOKEN_KEY
 
-def write_to_json_file(data, filename) -> None:
-    # writes the data to a json file to FILENAME
-    print(f"Creating {filename} ...")
-    with open(filename, 'w') as file_object:
-        json.dump(data, file_object)
-
+# TODO: Remove this at some point
+# def write_to_csv_file(data, filename) -> None:
+#     # writes the data to a json file to FILENAME
+#     print(f"Creating {filename} ...")
+#     with open(filename, 'w') as file_object:
+#         json.dump(data, file_object)
 
 def get_nyc_payroll_data(parameters) -> requests.Response:
     # requesting the data from the API endpoint
     print(f'Requesting NYC Payroll data ...')
-    response = requests.get('https://data.cityofnewyork.us/resource/k397-673e.json', params=parameters)
+    response = requests.get('https://data.cityofnewyork.us/resource/k397-673e.csv',      params=parameters)
 
     return response
 
+# TODO: Could probably merge download_data and get_nyc_payroll_data together
 
 def download_data(fiscal_year: int, limit: int, filename: str) -> None:
     # Uses write_to_json_file and get_nyc_payroll_data to create a json file with the data
@@ -29,15 +31,14 @@ def download_data(fiscal_year: int, limit: int, filename: str) -> None:
         }
 
     response = get_nyc_payroll_data(parameters)
-    data = response.json()
-
-    write_to_json_file(data)
+    print(response)
+    df = pd.DataFrame(response)
+    #df.to_csv(filename, index=False)
     
     print(f"{filename} successfully created!")
 
-def read_from_file(filename) -> List:
+# TODO Fix read from csv_file so it returns the dataframe of pandas
+def read_from_csv_file(filename):
     # Reads a json file and passes it
-    with open(filename, 'r') as file_object:
-        json_data = json.load(file_object)
-
-    return json_data
+    df = pd.read_csv(filename)
+    return df
