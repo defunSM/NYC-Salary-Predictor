56e2af6562e401b07883e1ab2012b8f99466424f
defunsm <salmanhossain500@gmail.com>
2021-08-15 17:37:17 -0400

    using asyncio and aiohttp instead of requests

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import aiohttp
+import asyncio
 
 from setup_nyc_payroll import download_data, read_from_file
 
@@ -10,14 +12,17 @@ def main():
 
     # Checks if the FILENAME has already been downloaded
     if not os.path.isfile(FILENAME):
+        
         download_data(FISCAL_YEAR, LIMIT, FILENAME)
-    else:
-        print(f"Skipping download {FILENAME} exists")
+    #else:
+      #  print(f"Skipping download {FILENAME} exists")
 
     # Taking the json data from FILENAME. 
 
-    payroll_data = read_from_file(FILENAME)
-    print(type(payroll_data))
+    #payroll_data = read_from_file(FILENAME)
+    # print(type(payroll_data))
+    
+    
 
 if __name__ == '__main__':
     main()
--- setup_nyc_payroll.py
+++ setup_nyc_payroll.py
@@ -1,8 +1,19 @@
 import requests
 import json
-from typing import List
+import asyncio
+import aiohttp
+import time
 
+from typing import List
 from secrets import API_TOKEN_KEY
+
+def timer(func):
+    # Used as a decorator to help time get_nyc_payroll_data
+    def wrapper(*args):
+        start = time.time()
+        func(args, args[-1])
+        print(time.time() - start)
+    return wrapper
 
 def write_to_json_file(data, filename) -> None:
     # writes the data to a json file to FILENAME
@@ -11,14 +22,18 @@ def write_to_json_file(data, filename) -> None:
         json.dump(data, file_object)
 
 
-def get_nyc_payroll_data(parameters) -> requests.Response:
+# TODO: Use aiohttp and asyncio to speed up this 
+
+async def get_nyc_payroll_data(parameters, filename):
     # requesting the data from the API endpoint
     print(f'Requesting NYC Payroll data ...')
-    response = requests.get('https://data.cityofnewyork.us/resource/k397-673e.json', params=parameters)
-
-    return response
-
-
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://data.cityofnewyork.us/resource/k397-673e.json', params=parameters) as response:
+            data = await response.json()
+            write_to_json_file(data, filename)
+            
+            
 def download_data(fiscal_year: int, limit: int, filename: str) -> None:
     # Uses write_to_json_file and get_nyc_payroll_data to create a json file with the data
     print(API_TOKEN_KEY)
@@ -27,15 +42,14 @@ def download_data(fiscal_year: int, limit: int, filename: str) -> None:
         '$$app_token': API_TOKEN_KEY,
         '$limit': limit,
         }
-
-    response = get_nyc_payroll_data(parameters)
-    data = response.json()
-
-    write_to_json_file(data)
+    
+    start = time.time()
+    asyncio.run(get_nyc_payroll_data(parameters, filename))
+    print(time.time() - start)
     
     print(f"{filename} successfully created!")
 
-def read_from_file(filename) -> List:
+def read_from_file(filename):
     # Reads a json file and passes it
     with open(filename, 'r') as file_object:
         json_data = json.load(file_object)
