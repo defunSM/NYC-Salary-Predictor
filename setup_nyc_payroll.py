9acf2f7deaf1b62d8566cca18c3bab1a3f764bc5
defunsm <salmanhossain500@gmail.com>
2022-02-01 20:35:29 -0500

    current

--- setup_nyc_payroll.py
+++ setup_nyc_payroll.py
@@ -1,27 +1,30 @@
-import requests
-import json
+
 import pandas as pd
+import requests
+
 
 from typing import List
 from secrets import API_TOKEN_KEY
 
-# TODO: Remove this at some point
-# def write_to_csv_file(data, filename) -> None:
-#     # writes the data to a json file to FILENAME
+
+# def write_to_csv_file(data, filename):
+   
 #     print(f"Creating {filename} ...")
 #     with open(filename, 'w') as file_object:
-#         json.dump(data, file_object)
+#         csv.dump(data, file_object)
 
-def get_nyc_payroll_data(parameters) -> requests.Response:
+def get_nyc_payroll_data(parameters):
     # requesting the data from the API endpoint
+    
     print(f'Requesting NYC Payroll data ...')
-    response = requests.get('https://data.cityofnewyork.us/resource/k397-673e.csv',      params=parameters)
-
+    
+    response = requests.get('https://data.cityofnewyork.us/resource/k397-673e.csv', params=parameters)
     return response
+
 
 # TODO: Could probably merge download_data and get_nyc_payroll_data together
 
-def download_data(fiscal_year: int, limit: int, filename: str) -> None:
+def download_data(fiscal_year: int, limit: int, filename: str):
     # Uses write_to_json_file and get_nyc_payroll_data to create a json file with the data
     print(API_TOKEN_KEY)
     parameters = {
@@ -31,13 +34,12 @@ def download_data(fiscal_year: int, limit: int, filename: str) -> None:
         }
 
     response = get_nyc_payroll_data(parameters)
-    print(response)
+    #print(response)
     df = pd.DataFrame(response)
-    #df.to_csv(filename, index=False)
+    df.to_csv(filename, index=False)
     
     print(f"{filename} successfully created!")
 
-# TODO Fix read from csv_file so it returns the dataframe of pandas
 def read_from_csv_file(filename):
     # Reads a json file and passes it
     df = pd.read_csv(filename)
